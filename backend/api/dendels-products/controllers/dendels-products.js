7cb83529bf3ed29156c175b9b0d038b3cca0287d
mitnikkozo <migirulz44@gmail.com>
2022-11-01 20:59:17 +1300

    Products get highest price

--- backend/api/dendels-products/controllers/dendels-products.js
+++ backend/api/dendels-products/controllers/dendels-products.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
 
+const { sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async updateprices(ctx) {
+    //get every product
+    //create a variable for highestPrice
+    //for every variant
+    //if variant highPrice is lower than the highestPrice replace it
+    //make the product highestPrice as highestPrice
+    //save and sanitize
+  },
+};
