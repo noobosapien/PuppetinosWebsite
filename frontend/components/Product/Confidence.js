8fce87773446d66155415d036f425c60580ea8c6
mitnikkozo <migirulz44@gmail.com>
2023-05-13 22:33:59 +1200

    Expectations

--- frontend/components/Product/Confidence.js
+++ frontend/components/Product/Confidence.js
@@ -9,18 +9,31 @@ export default function Confidence() {
         direction={'column'}
         justifyContent={'center'}
         alignItems={'center'}
+        spacing={4}
       >
         <Grid item>
           <Typography sx={{ fontWeight: 500 }}>We promise...</Typography>
         </Grid>
 
-        <Grid
-          item
-          container
-          direction={'column'}
-          spacing={2}
-          sx={{ marginLeft: '1px' }}
-        ></Grid>
+        <Grid item container spacing={2} justifyContent={'space-between'}>
+          <Grid item>
+            <Typography>Safe checkout</Typography>
+          </Grid>
+
+          <Grid item>
+            <Typography>Quality handiwork</Typography>
+          </Grid>
+        </Grid>
+
+        <Grid item container spacing={2} justifyContent={'space-between'}>
+          <Grid item>
+            <Typography>Privacy</Typography>
+          </Grid>
+
+          <Grid item>
+            <Typography>Exceed your expectations!</Typography>
+          </Grid>
+        </Grid>
       </Grid>
     </>
   );
